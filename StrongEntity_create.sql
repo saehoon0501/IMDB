6366a10f508d540a555a1462d356bef50c8e6742
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2021-06-28 22:29:20 +0900

    Create StrongEntity_create.sql

---
+++ StrongEntity_create.sql
@@ -0,0 +1,27 @@
+
+#Strong Entity 생성
+create table Movie(
+	title_id varchar(20),
+    title_type varchar(20),
+    primary_title varchar(200),
+    original_title varchar(200),
+    is_adult boolean,
+    start_year int,
+    end_year int,
+    runtime_minutes int,
+    primary key(title_id));
+    
+create table Person(
+	name_id varchar(20),
+    deathYear int,
+    birthYear int,
+    primary_name varchar(200),
+    primary key (name_id)
+    );
+    
+create table Review(
+	tconst varchar(20),
+    average_rating float,
+    num_votes int,
+    primary key (tconst));
+    
